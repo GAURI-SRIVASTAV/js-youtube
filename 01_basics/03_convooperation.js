7a3c93da7687f3c3f086b89fcb44c70d0a5a2067
GAURI-SRIVASTAV <141621918+GAURI-SRIVASTAV@users.noreply.github.com>
2024-03-13 19:12:36 +0000

    opertn

---
+++ 01_basics/03_convooperation.js
@@ -0,0 +1,51 @@
+let num ="10aa"
+//console.log(typeof num )
+//console.log(typeof(num));
+ 
+//conversion   starts with capital letter whereas datatype stats with small letter
+let value=Number(num)
+//console.log(value);
+    
+
+//10=>10
+//"10"=>10
+//"10aa"=>NaN...(Not A Number)
+// true => 1...false=>0
+
+
+bo =""
+let value1=Boolean(bo)
+//console.log(value1);
+
+//1 => true
+//0 => false
+//"xyz" => true
+//"" => false
+strg=33
+let value2=String(strg)
+//console.log(typeof value2);
+
+
+//.........OPERATIONS.........
+
+//arithmatic => +,-,*,/,**,%
+
+//string concatinate str1+str2
+
+ console.log(typeof("1" +2));  //12
+ console.log(1 +"2");  //12
+ console.log("1" +2+3);  //123
+ console.log(2+3+"4");  //54
+ console.log(2 +4+true);  //7
+ console.log(typeof(2 +4+true)) //number
+ console.log(true +2 +4 ); //7
+ console.log(typeof(true+2 +4)) //number
+
+
+ console.log(+true);   //1
+ 
+ console.log(+"")   //0
+
+
+ //  prefix increment  => ++value
+ //  postfix increment  =>  value++
