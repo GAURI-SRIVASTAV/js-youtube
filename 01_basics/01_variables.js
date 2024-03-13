0444944c4ddb02ebc25ebca5ad021d04ca89254e
GAURI-SRIVASTAV <141621918+GAURI-SRIVASTAV@users.noreply.github.com>
2024-03-13 18:04:30 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1443
+let accountEmail ="gauri@gmail.com"
+var accountPassword = "1111"
+accountCity = "Jaipur"
+  
+//accountId = 2......not allowed
+
+//SCOPE.....{
+
+}
+/* 
+PREFERED to use let over var to avoid  block scope issue fn scope 
+ */
+
+acconutCity="Goa"
+console.log(accountEmail)
+console.table([accountId,accountCity,accountPassword])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";// treat all JScode as newer version
+
+// alert(3+3) we are using node.js not browser
+
+console.log(3 +
+    3) // code readability is priority
+    
+//Prittier extension for code indent
+
+//Orginal documentation is at "tc39.ef".... i.e ...ECMAScript which specifies the standard of javascript
+
+//MDN Mozirilla Documentation for javascript
+
+/*DATATYPES
+
+  number
+  bigint
+  string
+  boolean
+  null => stand alone value
+  undefined
+  symbol => for uniqueness
+
+  object
+  */
+
+console.log(typeof undefined)  //undefined
+console.log(typeof null);  //object
